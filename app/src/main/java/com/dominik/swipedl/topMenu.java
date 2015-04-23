81eae47b1b0678499726cbac69a7692f28b6850e
dominik <lander45@hotmail.co.uk>
2015-04-23 17:31:58 +0100

    v0.1 with changes

--- app/src/main/java/com/dominik/swipedl/topMenu.java
+++ app/src/main/java/com/dominik/swipedl/topMenu.java
@@ -8,7 +8,6 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
-
 public class TopMenu extends ActionBarActivity implements View.OnClickListener {
 
     Button start_game_button;
