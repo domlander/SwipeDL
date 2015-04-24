a9ea7a15004f6d8939346ab9accccd06149e2668
dominik <lander45@hotmail.co.uk>
2015-04-24 16:52:53 +0100

    v0.1 with changes

--- app/src/main/java/com/dominik/swipedl/Game.java
+++ app/src/main/java/com/dominik/swipedl/Game.java
@@ -1,65 +1,99 @@
 package com.dominik.swipedl;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.graphics.PixelFormat;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.MotionEvent;
+import android.view.Surface;
+import android.view.SurfaceView;
 import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 public class Game extends ActionBarActivity {
 
-    // For a swipe to count, the player must be within ALLOWED_ANGLE degrees from vertical.
-    private final double ALLOWED_ANGLE_DEGREES = 15;
+    TextView countTV;
+    SurfaceView dragArea;
+    SurfaceView dragAreaBorder;
+    TextView debugText;
+    Button pauseButton;
 
     private boolean gameRunning = false;
     private int numSwipes = 0;
-    TextView countTV;
-    TextView debugText;
+    private final String UP = "UP";
+    private final String DOWN = "DOWN";
+    String swipeDirection;
+    boolean crossedNS; // if true, the user has changed swipe directions.
+
+    // Registers the co-ordinates of the user touching the screen and releasing.
+    double x_start, x_end;
+    double y_start, y_end;
+    double y_min, y_max;
+
+    SharedPreferences sharedPrefs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.game);
+
+        // Get settings: difficulty/game mode.
+        sharedPrefs = getSharedPreferences("Settings", Context.MODE_PRIVATE);
+        int numPlayers = sharedPrefs.getInt("numPlayers", 1);
+        boolean timeLimit = sharedPrefs.getBoolean("timeLimit", true);
+        int gameModeOption = sharedPrefs.getInt("gameModeOption", 3);
+        int difficulty = sharedPrefs.getInt("difficulty", 1);
+
+        setDragArea(difficulty);
     }
 
-    /*
-     * Timer that is called after every swipe. After 1 second the total
-     * number of swipes is displayed.
-     */
-    private CountDownTimer timer = new CountDownTimer(1000, 1000) {
-        @Override
-        public void onTick(long millisUntilFinished) {  }
+    private void setDragArea(int difficulty) {
 
-        @Override
-        public void onFinish() {
-            countTV = (TextView) findViewById(R.id.count);
-            countTV.setText(Integer.toString(numSwipes));
+        switch (difficulty) {
+            case 1: // easy
+            default:
+                resizeDragArea(600);
+                break;
+
+            case 2:
+                resizeDragArea(400);
+                break;
+
+            case 3: // hard
+                resizeDragArea(200);
+                break;
         }
-    };
-
-    /*
-     * Calculates the angle between the line between the two points:
-     * (x1,x2) and (y1,y2), and vertical line that intersects (y1,y2)
-     */
-    public double getAngle(double x1, double x2, double y1, double y2) {
-        if (y1 == y2) { return 0; }
-
-        return Math.acos((y2 - y1) / (Math.pow(Math.pow((y2 - y1), 2) + Math.pow((x2 - x1), 2), 0.5)));
     }
+
+    private void resizeDragArea(int width) {
+        dragArea = (SurfaceView) findViewById(R.id.dragArea);
+        dragArea.getHolder().setFixedSize(width, width);
+    }
+
+    public void onPauseClick(View v) {
+        //TODO
+    }
+
 
     public void onButtonClick(View v) {
         gameRunning = true;
-        timer.start();
     }
 
-    // Registers the co-ordinates of the user touching the screen and releasing.
-    double x_start, x_end;
-    double y_start, y_end;
-    double y_current; // current y value of the drag
-    boolean crossNS = false;
+    // "View" is the draggable box.
+//    View myView = findViewById(R.id.my_view);
+//    myView.setOnTouchListener(new OnTouchListener() {
+//        public boolean onTouch(View v, MotionEvent event) {
+//            // ... Respond to touch events
+//            return true;
+//        }
+//    });
 
     /*
      * Called when the user touches the screen.
@@ -70,71 +104,93 @@ public class Game extends ActionBarActivity {
 
         countTV = (TextView) findViewById(R.id.count);
 
-        // cancel the timer and hide the current score.
-        timer.cancel();
-        countTV.setText("");
-
         switch (swipe.getAction()) {
             case MotionEvent.ACTION_DOWN:
                 x_start = swipe.getX();
-                y_start = y_current = swipe.getY();
+                y_start = y_min = y_max = swipe.getY();
+                swipeDirection = "";
                 break;
 
             case MotionEvent.ACTION_MOVE:
-                if (!crossNS) {
-                    // if user starts dragging upwards the drag will not be valid
-                    if (swipe.getY() < y_start) {
-                        crossNS = true;
+
+                // if user has not already changed direction.
+                if (!crossedNS) {
+
+                    // set the initial direction of the drag.
+                    if (swipe.getY() > y_start) {
+                        swipeDirection = DOWN;
+                    } else if (swipe.getY() < y_start) {
+                        swipeDirection = UP;
                     }
-                    // When the user stops dragging down, the north-south
-                    // divide has been crossed and the drag ends.
-                    if (swipe.getY() >= y_current) {
-                        y_current = swipe.getY();
-                    } else {
-                        crossNS = true;
-                        if (isLegalDrag(swipe)) {
+
+                    // if the drag is going down and the current y value is higher than
+                    // the lowest recorded value of y, then the direction has changed.
+                    if (swipeDirection.equals(DOWN)) {
+                        if (swipe.getY() <= y_max) {
+                            crossedNS = true;
+                        } else {
+                            y_max = swipe.getY(); // Update y_max with current value.
+                        }
+
+                    // The same as above but with the initial drag direction going up.
+                    } else if (swipeDirection.equals(UP)) {
+                        if (swipe.getY() >= y_min) {
+                            crossedNS = true;
+                        } else {
+                            y_min = swipe.getY(); // Update y_min with current value.
+                        }
+                    }
+
+                    // If the north-south divide has been crossed the drag ends.
+                    if (crossedNS = true) {
+                        x_end = swipe.getX();
+                        y_end = swipe.getY();
+                        if (isLegalDrag()) {
                             numSwipes += 1;
                         }
                     }
                 }
-                //debugText(swipe);
+                debugText(swipe);
                 break;
 
             case MotionEvent.ACTION_UP:
-                x_end = swipe.getX();
-                y_end = swipe.getY();
-
-                if ((!crossNS) && (isLegalDrag(swipe))) {
-                    numSwipes += 1;
+                if (!crossedNS) {
+                    x_end = swipe.getX();
+                    y_end = swipe.getY();
+                    if ((isLegalDrag())) {
+                        numSwipes += 1;
+                    }
+                    crossedNS = false;
                 }
-                crossNS = false;
+                debugText(swipe);
                 break;
         }
-        timer.start();
+        countTV.setText(Integer.toString(numSwipes));
         return false;
     }
 
-    // Legal drag if end point is lower than start point and the angle is within the specified
-    // allowed limit. Converts degrees to radians.
-    private boolean isLegalDrag(MotionEvent swipe) {
-        x_end = swipe.getX();
-        y_end = swipe.getY();
-        return ((y_end > y_start) &&
-                (getAngle(x_start, x_end, y_start, y_end) <= ALLOWED_ANGLE_DEGREES * 0.0174532925));
+    // Finds gradient. If angle between drag and vertical line is <15degrees, drag is legal.
+    private boolean isLegalDrag() {
+        if ((y_start == y_end) || (x_start == x_end)) { return false; }
+
+        return Math.abs((y_end - y_start) / (x_end - x_start)) >= 3.73205; // 15 degrees from vertical
     }
 
     // Prints stats to screen for debugging purposes
     private void debugText(MotionEvent swipe) {
         debugText = (TextView) findViewById(R.id.debugText);
         debugText.setText(
-                String.format("%.2f", x_start) + " " +
-                String.format("%.2f", x_end) + " " +
-                String.format("%.2f", y_start) + " " +
-                String.format("%.2f", y_end) + " " +
-                String.format("%.2f", getAngle(x_start, x_end, y_start, y_end)) + " " +
-                String.format("%.2f", swipe.getY()) + " " +
-                String.format("%.2f", y_current) + " " +
-                Boolean.toString(crossNS));
+            "x1: " + String.format("%.2f", x_start) +
+            " x2: " + String.format("%.2f", x_end) +
+            " y1: " + String.format("%.2f", y_start) +
+            " y2: " + String.format("%.2f", y_end) +
+            " yMin: " + String.format("%.2f", y_min) +
+            " yMax: " + String.format("%.2f", y_max) +
+            " grad: " + String.format("%.2f", Math.abs((y_end - y_start) / (x_end - x_start))) +
+            " y: " + String.format("%.2f", swipe.getY()) +
+            " Swipe Direction: " + swipeDirection +
+            " crossedNS: " + Boolean.toString(crossedNS)
+        );
     }
 
     @Override
